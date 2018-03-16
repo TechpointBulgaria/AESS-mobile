2f2655ea23fdf509227d2b24405030335d1cedc2
hukuuu <hukuuu@gmail.com>
2018-03-16 14:35:38 +0200

    added linear gradient library

--- android/app/src/main/java/com/mqttmobile/MainApplication.java
+++ android/app/src/main/java/com/mqttmobile/MainApplication.java
@@ -3,6 +3,8 @@ package com.mqttmobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.horcrux.svg.SvgPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LinearGradientPackage(),
+            new ReactNativeConfigPackage(),
             new SvgPackage(),
             new VectorIconsPackage()
       );
