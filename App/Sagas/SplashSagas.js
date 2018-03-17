ba0cb9f7a2acf6f72b83fa4f061ea22906675546
hukuuu <hukuuu@gmail.com>
2018-03-17 21:55:49 +0200

    make splash screen wait a little

--- App/Sagas/SplashSagas.js
+++ App/Sagas/SplashSagas.js
@@ -11,11 +11,15 @@
  *************************************************************/
 
 import { delay } from 'redux-saga'
-import { call, put, select } from 'redux-saga/effects'
+import { all, call, put, select } from 'redux-saga/effects'
 import SplashActions from '../Redux/SplashRedux'
 import { AsyncStorage } from 'react-native'
 import RoomActions from '../Redux/RoomRedux'
 import { LoginSelectors } from '../Redux/LoginRedux'
+
+function* waitRandomTime() {
+  yield call(delay, 800 + Math.random() * 800)
+}
 
 export function* init(api, action) {
   let rehydrated = yield select(
@@ -31,12 +35,18 @@ export function* init(api, action) {
   const token = yield select(LoginSelectors.getToken)
 
   if (token) {
-    const roomsResponse = yield call(api.fetchRooms, token)
+    const [roomsResponse] = yield all([
+      call(api.fetchRooms, token),
+      call(waitRandomTime)
+    ])
     if (roomsResponse.ok) {
       yield put(RoomActions.roomSuccess(roomsResponse.data))
     } else {
       yield put(RoomActions.roomFailure())
     }
     yield put(SplashActions.loggedIn())
-  } else yield put(SplashActions.notLoggedIn())
+  } else {
+    yield call(waitRandomTime)
+    yield put(SplashActions.notLoggedIn())
+  }
 }
